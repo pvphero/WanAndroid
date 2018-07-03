ad20ce91e4f100e6c9f9ec8c891fed7213f3ab41
Roy.Shen <Roy.Shen@homecreditcfc.cn>
2018-07-03 10:26:10 +0800

    base

--- app/src/main/java/com/vv/wanandroid/ui/base/BaseAbListFragment.java
+++ app/src/main/java/com/vv/wanandroid/ui/base/BaseAbListFragment.java
@@ -12,6 +12,7 @@ import com.vv.wanandroid.ui.adapter.BaseListAdapter;
 import com.vv.wanandroid.ui.mvp.view.IListDataView;
 import com.vv.wanandroid.ui.mvp.view.IView;
 import com.vv.wanandroid.utils.LogUtils;
+import com.vv.wanandroid.utils.ToastUtils;
 import com.vv.wanandroid.widget.ContainerLayout;
 import com.vv.wanandroid.widget.LMRecyclerView;
 
@@ -91,10 +92,8 @@ public abstract class BaseAbListFragment<P extends BasePresenter<V>, V extends I
         isAutoLoadMore = true;
         page = 0;
         loadDatas();
-        LogUtils.i(TAG,"下拉刷新...");
+        LogUtils.iTag(TAG,"下拉刷新...");
     }
-
-    protected abstract void loadDatas();
 
     @Override
     public void loadMore() {
@@ -103,13 +102,92 @@ public abstract class BaseAbListFragment<P extends BasePresenter<V>, V extends I
         }
         state = Const.PAGE_STATE.SATE_LOAD_MORE;
         loadDatas();
-        LogUtils.i(TAG,"加载更多...");
+        LogUtils.iTag(TAG,"加载更多...");
     }
 
     @Override
     public void reloadMore() {
         isAutoLoadMore=true;
         loadMore();
-        LogUtils.i(TAG,"重新加载更多...");
+        LogUtils.iTag(TAG,"重新加载更多...");
     }
+
+    @Override
+    public void clearListData() {
+        mListData.clear();
+        LogUtils.iTag(TAG,mListData.size()+"条数据被清空");
+    }
+
+    @Override
+    public void showNoMore() {
+        mRecyclerView.showNoMoreData();
+        isAutoLoadMore=true;
+    }
+
+    @Override
+    public void showError() {
+        isAutoLoadMore = false;
+        if (state == Const.PAGE_STATE.SATE_LOAD_MORE) {
+            mRecyclerView.showLoadMoreError();
+            LogUtils.iTag(TAG, "加载数据出错,显示重新加载..");
+        } else {
+            mContainerLayout.showError();
+            LogUtils.iTag(TAG, "请求失败,显示没有数据");
+        }
+    }
+
+    @Override
+    public void showEmpty() {
+        mContainerLayout.showEmpty();
+        LogUtils.iTag(TAG,"无数据");
+    }
+
+    @Override
+    public int getPage() {
+        LogUtils.iTag(TAG,"页数为:"+page);
+        return page;
+    }
+
+    @Override
+    public void showLoading(String msg) {
+        if (state == Const.PAGE_STATE.STATE_REFRESH) {
+            setRefreshing(true);
+        }
+    }
+
+    @Override
+    public void hideLoading() {
+        setRefreshing(false);
+    }
+
+    @Override
+    public void showFail(String msg) {
+        ToastUtils.showToast(getActivity(),msg);
+        LogUtils.iTag(TAG,"失败");
+    }
+
+    private void setRefreshing(final boolean isRefreshing) {
+        mRefreshlayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mRefreshlayout.setRefreshing(isRefreshing);
+            }
+        }, 100);
+    }
+
+    @Override
+    protected void receiveEvent(Object o) {
+
+    }
+
+    @Override
+    protected String registerEvent() {
+        return null;
+    }
+
+    protected abstract void loadDatas();
+
+    protected abstract View initHeardView();
+
+    protected abstract BaseListAdapter getListAdapter();
 }
